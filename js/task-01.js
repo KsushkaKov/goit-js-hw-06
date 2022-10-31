3845e045b6cd8de823f5f442052aec1a9108d9ea
Ksenia Kovalenko <ksushka.ua@gmail.com>
2022-10-31 20:01:58 +0200

    Done first 3 tasks

---
+++ js/task-01.js
@@ -0,0 +1,7 @@
+const categoryList = document.querySelectorAll(".item");
+console.log(`Number of categories: ${categoryList.length}`);
+
+categoryList.forEach((item) => {
+  console.log(`Category: ${item.firstElementChild.textContent}`);
+  console.log(`Elements: ${item.lastElementChild.children.length}`);
+});
